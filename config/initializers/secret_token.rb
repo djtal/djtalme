65a13d3a19ebc7b469e455858e6714a82c00a578
djtal <guillaume.garcera@gmail.com>
2012-09-24 22:58:59 +0200

    initial work. setup bootstrap layout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Djtalme::Application.config.secret_token = '55ded48308dd5f94f5e6f9227a3259bd250d8a1246210f30ac2c1db8b59f2c6f1615e9ddfd0218f1d7d5c6730251a5f845d6b66bed34d5490113e25cadee823e'
